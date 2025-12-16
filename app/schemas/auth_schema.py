4268879f037b9c508fe1c76817afed06e669334f
Gabriel Claudino <90232353+Claudino2001@users.noreply.github.com>
2025-12-15 22:13:30 -0300

    Add validation for username and password in UserCreate
    Introduced field constraints for username and password in the UserCreate schema, including length restrictions and a custom password validator to enforce at least one uppercase letter, one lowercase letter, and one number.

--- app/schemas/auth_schema.py
+++ app/schemas/auth_schema.py
@@ -1,10 +1,20 @@
-from pydantic import BaseModel, EmailStr
-
+from pydantic import BaseModel, EmailStr, Field, field_validator
+import re
 
 class UserCreate(BaseModel):
-    username: str
+    username: str = Field(..., min_length=3, max_length=25, description="Nome de usuário entre 3 e 25 caracteres")
     email: EmailStr
-    password: str
+    password: str = Field(..., min_length=8, max_length=16, description="Senha entre 8 e 16 caracteres")
+
+    @field_validator('password')
+    def validate_password_strength(cls, v):
+        if not re.search(r'[A-Z]', v):
+            raise ValueError('A senha deve conter pelo menos uma letra maiúscula.')
+        if not re.search(r'[a-z]', v):
+            raise ValueError('A senha deve conter pelo menos uma letra minúscula.')
+        if not re.search(r'[0-9]', v):
+            raise ValueError('A senha deve conter pelo menos um número.')
+        return v
 
 
 class LoginRequest(BaseModel):

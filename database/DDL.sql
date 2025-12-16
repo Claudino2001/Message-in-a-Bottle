a9154b55dea62d5c2c887cd124c54158c3ba1168
Gabriel Claudino <90232353+Claudino2001@users.noreply.github.com>
2025-12-16 18:19:07 -0300

    Add is_verified column to users table
    Introduces an is_verified boolean column to the users table to track whether a user's email or account has been verified. The default value is set to FALSE.

--- database/DDL.sql
+++ database/DDL.sql
@@ -10,7 +10,8 @@ CREATE TABLE users (
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
-    language VARCHAR(10) DEFAULT 'pt-BR', -- Para suportar múltiplos idiomas
+    language VARCHAR(10) DEFAULT 'pt-BR',
+    is_verified BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
